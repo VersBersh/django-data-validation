56259e43744e87123478cb9ef9a8bfc880db56a6
Oliver Chambers <o.chambers2@gmail.com>
2020-06-24 09:14:22 +1000

    first production build
    remove some of the default content from create-react-app and do an `npm
    run build`

---
+++ datavalidation/static/datavalidation/precache-manifest.c47fe53c5476dd8366b0e25904bf5bb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9678d07c3cce91219dc6",
+    "url": "/static/css/main.chunk.css"
+  },
+  {
+    "revision": "0740651db3deb04302db",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9678d07c3cce91219dc6",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "c9a235b638b5b0cf5dda",
+    "url": "/static/js/runtime-main.js"
+  }
+]);
