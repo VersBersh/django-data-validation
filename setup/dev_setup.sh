7d34b04cfb81a89ad8ac384efbedf4ef3e2622e7
Oliver Chambers <o.chambers2@gmail.com>
2020-07-07 22:50:33 +1000

    move tests/test_proj -> /test_proj

--- setup/dev_setup.sh
+++ setup/dev_setup.sh
@@ -2,8 +2,7 @@
 
 # Quick and Dirty script to setup the dev environment
 # activate a virtualenv before running this script
-
-SETUP_DIR="$(dirname "${BASH_SOURCE[0]}")"
+SETUP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 PROJ_ROOT="$(dirname "$SETUP_DIR")"
 
 if [[ -z "$VIRTUAL_ENV" ]]; then
@@ -14,23 +13,23 @@ fi
 # add project paths to site-packages
 pypths="$SETUP_DIR/test_proj.pth"
 cat >"$pypths" <<-EOF
-	$PROJ_ROOT/tests/
-	$PROJ_ROOT/tests/test_proj
-	$PROJ_ROOT/tests/test_proj/apps
+	$PROJ_ROOT
+	$PROJ_ROOT/test_proj
+	$PROJ_ROOT/test_proj/apps
 	EOF
 
-sitepackages="$VIRTUAL_ENV/lib/python*/site-packages"
-if [[ -d "$sitepackages" ]]; then
-  ln -s "$pypths" .
-else
-  printf "cannot find site-packages at %s" "$sitepackages"
-  return 1
+sitepackages=("$VIRTUAL_ENV"/lib/python*/site-packages/)
+if ! [[ "${#sitepackages[@]}" -eq 1 ]]; then
+	printf "cannot find site-packages at %s\n" "$VIRTUAL_ENV/lib/python"
+  exit 1
+elif ! [[ -e "${sitepackages[0]}/test_proj.pth" ]]; then
+  ln -s "$pypths" "${sitepackages[0]}"
 fi
 unset pypths
 
 # symlink datavalidation app into tests
 # pycharm recommendation: right click and mark the symlink as excluded
-ln -s "$PROJ_ROOT/datavalidation/" "$PROJ_ROOT/tests/test_proj/apps/"
+ln -s "$PROJ_ROOT/datavalidation/" "$PROJ_ROOT/test_proj/apps/"
 
 # install requirements
 pip install -r "$PROJ_ROOT/deps/pip.base"
@@ -39,4 +38,5 @@ pip install -r "$PROJ_ROOT/deps/pip.base"
 nodeenv -p
 
 # install the node modules
+cd datavalidation/admin || exit
 npm install
